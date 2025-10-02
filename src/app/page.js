3af6bc656e4e0f4ba9553934e3b68124786cfdc2
RomitWorkSpace <romitkumar848@gmail.com>
2025-10-02 11:08:01 +0530

    Recent design component added

--- src/app/page.js
+++ src/app/page.js
@@ -5,24 +5,26 @@ import Header from "./components/home/header";
 import MainArea from "./components/home/main-area";
 import Sidebar from "./components/home/sidebar";
 import { useTshirtStore } from "./store/index.js";
+import RecentDesign from "./components/home/recent-design";
 
 export default function Home() {
 
-  const tshirtType = useTshirtStore((state) => state.tshirtType)
+  // const tshirtType = useTshirtStore((state) => state.tshirtType)
 
   return (
     <>
     <Header />
     <div className="max-w-7xl mx-auto">
-    <div className="max-h-screen w-full overflow-hidden">
+    <div className="w-full overflow-hidden">
       <div className="flex">
         <div className="flex-1">
           <MainArea />
         </div>
-        <div className={`realtive ${tshirtType ? 'w-1/4' : 'w-0'}`}>
+        {/* <div className={`home-sidebar relative ${tshirtType ? 'w-1/4' : 'w-0'}`}>
           <Sidebar />
-        </div>
+        </div> */}
       </div>
+      <RecentDesign />
     </div>
     </div>
     </>
